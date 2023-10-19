6fa0d87871e2aa21feeb635d66b04d042ba60ec1
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-10-19 09:08:14 +0900

    Initial Commit

---
+++ ChatClientForm/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace ChatClientForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnDisconnect = new Button();
+            SuspendLayout();
+            // 
+            // btnDisconnect
+            // 
+            btnDisconnect.Location = new Point(299, 12);
+            btnDisconnect.Name = "btnDisconnect";
+            btnDisconnect.Size = new Size(75, 23);
+            btnDisconnect.TabIndex = 0;
+            btnDisconnect.Text = "연결끊기";
+            btnDisconnect.UseVisualStyleBackColor = true;
+            btnDisconnect.Click += BtnDisConnect_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(386, 332);
+            Controls.Add(btnDisconnect);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnDisconnect;
+    }
+}
---
+++ ChatClientForm/Form1.cs
@@ -0,0 +1,15 @@
+namespace ChatClientForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnDisConnect_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
