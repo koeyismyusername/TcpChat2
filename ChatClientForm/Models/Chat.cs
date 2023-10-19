d3533d65c5b3a83113e2981f689efd71d8bd1295
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-10-19 11:26:27 +0900

    Feat: Chat 구현

---
+++ ChatClientForm/Models/Chat.cs
@@ -0,0 +1,11 @@
+﻿using ChatClientForm.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClientForm.Models
+{
+    public record Chat(long roomId, string username, string message, ChatState state);
+}
